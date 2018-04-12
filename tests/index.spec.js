16a55fa5a969dfad0ad22b7d13ce37e5c0ca10a1
Dmitri Zagidulin <dzagidulin@gmail.com>
2018-04-12 16:13:58 -0400

    Add injector test.

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -4,8 +4,36 @@ chai.should();
 
 const {expect} = chai;
 
-describe('dids', () => {
-  it('should exist', () => {
-    expect(require('../lib/index')).to.exist();
+const dids = require('../lib/index');
+const jsonld = require('jsonld')();
+const jsigs = require('jsonld-signatures');
+jsigs.use('jsonld', jsonld);
+// const eproofs = require('equihash-signature');
+// eproofs.install(jsigs);
+
+describe('dids api', () => {
+  it('accept JSON-LD libs via injector', () => {
+    dids.use('jsonld', jsonld);
+    dids.use('jsonld-signatures', jsigs);
+
+    const v1 = dids.methods.veres();
+
+    expect(v1.injector._libs).to.have.property('jsonld');
+  });
+});
+
+describe('did methods', () => {
+  let v1;
+
+  before(() => {
+    dids.use('jsonld', jsonld);
+    dids.use('jsonld-signatures', jsigs);
+    v1 = dids.methods.veres();
+  });
+
+  describe('veres one', () => {
+    it('should exist', () => {
+      expect(v1.ledger).to.equal('veres');
+    });
   });
 });
