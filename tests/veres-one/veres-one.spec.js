1a8ef033b68644340de154ec13c8e4cdf2f4656c
Dmitri Zagidulin <dzagidulin@gmail.com>
2018-04-18 15:59:47 -0400

    Add v1 contexts, wrap() and tests.

--- tests/veres-one/veres-one.spec.js
+++ tests/veres-one/veres-one.spec.js
@@ -36,32 +36,53 @@ describe('methods/veres-one', () => {
           .privateKey.privateKeyPem)
         .to.have.string('-----BEGIN ENCRYPTED PRIVATE KEY-----');
     }).timeout(30000);
+
+    it('should generate unprotected nym-based DID Document', async () => {
+      const nymOptions = {
+        passphrase: null
+      };
+      const didDocument = await v1.generate(nymOptions);
+
+      expect(didDocument.publicDidDocument.id).to.match(
+        /^did\:v1\:test\:nym\:.*/);
+      expect(
+        didDocument.publicDidDocument.authentication[0].publicKey[0].publicKeyPem)
+        .to.have.string('-----BEGIN PUBLIC KEY-----');
+      expect(
+        didDocument.privateDidDocument.authentication[0].publicKey[0]
+          .privateKey.privateKeyPem)
+        .to.have.string('-----BEGIN RSA PRIVATE KEY-----');
+    }).timeout(30000);
+
+    it('should generate uuid-based DID Document', async () => {
+      const uuidOptions = {
+        didType: 'uuid'
+      };
+      const didDocument = await v1.generate(uuidOptions);
+
+      expect(didDocument.publicDidDocument.id).to.match(
+        /^did\:v1\:test\:uuid\:.*/);
+    });
   });
 
-  it('should generate unprotected nym-based DID Document', async () => {
-    const nymOptions = {
-      passphrase: null
-    };
-    const didDocument = await v1.generate(nymOptions);
+  describe('wrap', () => {
+    it('should wrap a nym-based DID Document in an operation', async () => {
+      const {publicDidDocument: didDocument} = await v1.generate(
+        {passphrase: null});
 
-    expect(didDocument.publicDidDocument.id).to.match(
-      /^did\:v1\:test\:nym\:.*/);
-    expect(
-      didDocument.publicDidDocument.authentication[0].publicKey[0].publicKeyPem)
-      .to.have.string('-----BEGIN PUBLIC KEY-----');
-    expect(
-      didDocument.privateDidDocument.authentication[0].publicKey[0]
-        .privateKey.privateKeyPem)
-      .to.have.string('-----BEGIN RSA PRIVATE KEY-----');
-  }).timeout(30000);
+      const operation = v1.wrap({didDocument});
+      expect(operation.type).to.equal('CreateWebLedgerRecord');
+      expect(operation.record.id).to.match(/^did\:v1\:test\:nym\:.*/);
+      expect(operation.record.authentication[0].publicKey[0].publicKeyPem)
+        .to.have.string('-----BEGIN PUBLIC KEY-----');
+    }).timeout(30000);
 
-  it('should generate uuid-based DID Document', async () => {
-    const uuidOptions = {
-      didType: 'uuid'
-    };
-    const didDocument = await v1.generate(uuidOptions);
-
-    expect(didDocument.publicDidDocument.id).to.match(
-      /^did\:v1\:test\:uuid\:.*/);
+    it('should wrap a uuid-based DID Document in an operation', async () => {
+      const {publicDidDocument: didDocument} = await v1.generate(
+        {didType: 'uuid'});
+      const operation = v1.wrap({didDocument});
+      expect(operation.type).to.equal('CreateWebLedgerRecord');
+      expect(operation.record.id).to.match(/^did\:v1\:test\:uuid\:.*/);
+    });
   });
 });
