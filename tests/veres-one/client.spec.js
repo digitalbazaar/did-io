f788d8d6d7155ee211bb62fc10864f2b8e731875
Dmitri Zagidulin <dzagidulin@gmail.com>
2018-04-20 13:21:32 -0400

    Refactor client.get() and test.

--- tests/veres-one/client.spec.js
+++ tests/veres-one/client.spec.js
@@ -5,13 +5,14 @@ chai.should();
 
 const {expect} = chai;
 
+const url = require('url');
 const tls = require('tls');
 tls.DEFAULT_ECDH_CURVE = 'auto';
 
 const VeresOneClient = require('../../lib/methods/veres-one/client');
 
-const TEST_DID = 'did:v1:test:nym:QdF43dq9Qu5HrDcMq91hebewWK5bvVWQ4CeyRrQ5Ydq';
-const TEST_DID_DOC = require('../dids/genesis.testnet.did.json');
+const TEST_DID = 'did:v1:test:nym:2pfPix2tcwa7gNoMRxdcHbEyFGqaVBPNntCsDZexVeHX';
+const TEST_DID_RESULT = require('../dids/genesis.testnet.did.json');
 const LEDGER_AGENTS_DOC = require('../dids/ledger-agents.json');
 
 describe('did methods', () => {
@@ -22,28 +23,33 @@ describe('did methods', () => {
   });
 
   describe('veres one client', () => {
-    describe('get', () => {
-      it('should fetch a did doc from ledger via https', async () => {
-        nock('https://example.com')
-          .get(`/dids/${TEST_DID}`)
-          .reply(200, TEST_DID_DOC);
-
-        const result = await client.get(TEST_DID, 'example.com');
-        // console.log(JSON.stringify(result, 0, 2));
-        expect(result.doc.id).to.equal(TEST_DID);
-      });
-    });
-
     describe('getAgent', () => {
       it('should resolve with an agent url for a ledger', async () => {
         nock('https://genesis.testnet.veres.one')
           .get(`/ledger-agents`)
           .reply(200, LEDGER_AGENTS_DOC);
 
-        const agent = await client.getAgent({mode: 'test'});
+        const agent = await client.getAgent({ mode: 'test' });
         expect(agent.id.startsWith('urn:uuid:')).to.be.true();
         const {ledgerConfigService} = agent.service;
         expect(ledgerConfigService.endsWith('/config')).to.be.true();
+      });
+    });
+
+    describe('get', () => {
+      it('should fetch a did doc from ledger via https', async () => {
+        nock('https://genesis.testnet.veres.one')
+          .get(`/ledger-agents`)
+          .reply(200, LEDGER_AGENTS_DOC);
+
+        nock('https://genesis.testnet.veres.one')
+          .post('/ledger-agents/72fdcd6a-5861-4307-ba3d-cbb72509533c' +
+               '/query?id=' + TEST_DID)
+          .reply(200, TEST_DID_RESULT);
+
+        const result = await client.get(TEST_DID);
+        // console.log(JSON.stringify(result, 0, 2));
+        expect(result.doc.object.id).to.equal(TEST_DID);
       });
     });
 
