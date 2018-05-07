318fe11cd353ba1f4cffd12e9cd557c845813bfe
Dmitri Zagidulin <dzagidulin@gmail.com>
2018-05-07 15:00:54 -0400

    Refactor didDoc.init() to set key ids.

--- tests/veres-one/veres-did-doc.spec.js
+++ tests/veres-one/veres-did-doc.spec.js
@@ -6,6 +6,7 @@ chai.should();
 const {expect} = chai;
 
 const Injector = require('../../lib/Injector');
+const {LDKeyPair} = require('../../lib/ld-key-pair');
 const injector = new Injector();
 injector.env = {nodejs: true};
 
@@ -38,36 +39,44 @@ describe('VeresOneDidDoc', () => {
 
     it('should init the did id', async () => {
       const env = 'dev';
-      sinon.stub(didDoc, 'generateKeys').resolves();
-      sinon.stub(didDoc, 'initId');
-      sinon.stub(didDoc, 'initSuites');
+      sinon.spy(didDoc, 'generateId');
 
       await didDoc.init({env});
 
-      expect(didDoc.initId).to.have.been.calledWith(env);
+      expect(didDoc.generateId).to.have.been.called();
     });
 
     it('should init the authn/authz suites', async () => {
       const env = 'dev';
-      sinon.stub(didDoc, 'generateKeys').resolves();
-      sinon.stub(didDoc, 'initId');
-      sinon.stub(didDoc, 'initSuites');
 
       await didDoc.init(env);
 
-      expect(didDoc.initSuites).to.have.been.called();
+      expect(didDoc.doc.authentication.length).to.equal(1);
+      expect(didDoc.doc.grantCapability.length).to.equal(1);
+      expect(didDoc.doc.invokeCapability.length).to.equal(1);
     });
   });
 
-  describe('initId', () => {
+  describe('generateId', () => {
     const keyType = 'Ed25519VerificationKey2018';
 
-    it('should init a uuid type did', async () => {
+    it('should generate a uuid type did', async () => {
       const didDoc = new VeresOneDidDoc({keyType, didType: 'uuid', injector});
-      await didDoc.generateKeys();
-      didDoc.initId('dev');
+      const did = didDoc.generateId({env: 'dev'});
 
-      expect(didDoc.id).to.match(/^did:v1:test:uuid:.*/);
+      expect(did).to.match(/^did:v1:test:uuid:.*/);
+    });
+
+    it('should generate a nym type did', async () => {
+      const didDoc = new VeresOneDidDoc({keyType, didType: 'nym', injector});
+      const keyOptions = {
+        keyType, injector: didDoc.injector, passphrase: null
+      };
+
+      const authKey = await LDKeyPair.generate(keyOptions);
+      const did = didDoc.generateId({authKey, env: 'dev'});
+
+      expect(did).to.match(/^did:v1:test:nym:.*/);
     });
   });
 
