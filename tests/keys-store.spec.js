77072173b53ecd9c78c487d79ccabb3aeceb58e4
Dmitri Zagidulin <dzagidulin@gmail.com>
2018-05-07 17:25:35 -0400

    Add LDKeyPair.from(), add round-trip import/export tests.

--- tests/keys-store.spec.js
+++ tests/keys-store.spec.js
@@ -8,36 +8,5 @@ const {expect} = chai;
 
 const KeyStore = require('../lib/key-store');
 
-describe.skip('ld-signatures keys', () => {
-  const keys = new KeyStore();
-
-  describe('generateEd25519KeyPair', () => {
-    it('should generate a key pair', () => {
-      const keyPair = keys.generateEd25519KeyPair({});
-      expect(keyPair).to.have.property('publicKey');
-      expect(keyPair).to.have.property('secretKey');
-    });
-  });
-
-  describe('generateRsaKeyPair', () => {
-    it('should generate a key pair', async() => {
-      const keyPair = await keys.generateRsaKeyPair();
-      expect(keyPair).to.have.property('publicKey');
-      expect(keyPair).to.have.property('secretKey');
-    });
-  });
-
-  describe('encrypt/decrypt', () => {
-    it.skip('should do a roundtrip encrypt/decrypt', async() => {
-      const {privateKey} = await keys.generateRsaKeyPair();
-      const passphrase = 'test';
-      const jwe = await keys.encrypt(privateKey, passphrase);
-
-      expect(jwe.unprotected.jwk.kty).to.equal('PBKDF2');
-
-      const decryptedKey = await keys.decrypt(jwe, passphrase);
-
-      expect(decryptedKey).to.equal(privateKey);
-    });
-  });
+describe.skip('KeyStore', () => {
 });
