a0b32a7dea42f37d4a8a29ea4d0302420b0ada4f
Dmitri Zagidulin <dzagidulin@gmail.com>
2021-03-17 15:48:20 -0400

    Refactor, update tests.

--- src/main.js
+++
@@ -1,7 +0,0 @@
-/*!
- * Copyright (c) 2020-2021 Digital Bazaar, Inc. All rights reserved.
- */
-export {
-  DidResolver
-} from './DidResolver.js';
-export {VERIFICATION_RELATIONSHIPS} from './constants.js';
