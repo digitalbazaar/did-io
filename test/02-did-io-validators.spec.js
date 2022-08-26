13a9c6c006f60f9e7e42006d7a354dc33dc3f585
Andrew Jones <ajones@digitalbazaar.com>
2022-08-26 10:10:35 -0400

    Bump copyright to 2022.
    Co-authored-by: David I. Lehn <dlehn@digitalbazaar.com>

--- test/02-did-io-validators.spec.js
+++ test/02-did-io-validators.spec.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
  */
 import chai from 'chai';
 import {testDid} from './helpers.js';
