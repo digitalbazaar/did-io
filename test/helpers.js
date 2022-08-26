8c93c990263d2770cf886d28de583d4e833b8669
Andrew Jones <ajones@digitalbazaar.com>
2022-08-26 10:11:04 -0400

    Bump Copyright to 2022.
    Co-authored-by: David I. Lehn <dlehn@digitalbazaar.com>

--- test/helpers.js
+++ test/helpers.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
  */
 import {validateDid} from '../lib/validators.js';
 
